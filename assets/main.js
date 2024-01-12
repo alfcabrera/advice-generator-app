f5d558afdc20a575ed817b779263125881cc4f2c
Mario Cabrera <mac25us@gmail.com>
2024-01-12 10:37:08 -0400

    🎨 improve function structure and added onLoad event

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,16 @@
 const adviceIdEl = document.querySelector("#advice-id");
 const adviceTextEl = document.querySelector("#advice-content");
 const rollTheDiceBtn = document.querySelector("#roll");
-const url = "https://api.adviceslip.com/advice";
+const URL = "https://api.adviceslip.com/advice";
 
-fetch(url, { cache: "no-cache" })
-    .then((response) => response.json())
-    .then((data) => {
-        adviceIdEl.innerHTML = `Advice #${data.slip.id}`;
-        adviceTextEl.innerHTML = data.slip.advice;
-    })
-    .catch((err) => console.error(err));
-
-rollTheDiceBtn.addEventListener("click", function (e) {
-    fetch(url, { cache: "no-cache" })
+function rollTheDice() {
+    fetch(URL, { cache: "no-cache" })
         .then((response) => response.json())
         .then((data) => {
             adviceIdEl.innerHTML = `Advice #${data.slip.id}`;
             adviceTextEl.innerHTML = data.slip.advice;
-        });
-});
+        })
+        .catch((err) => console.error(err));
+}
+
+rollTheDiceBtn.addEventListener("click", rollTheDice);
